c3102f23125efe89514c5bc8726751c9a027ed7f
Gannon Murray <gbmurray21@earlham.edu>
2024-06-27 15:34:54 -0400

    Reverted and updated visualization Removed redundancies

--- ampere.py
+++ ampere.py
@@ -2,7 +2,7 @@ import argparse
 import numpy as np
 import pandas as pd
 import plotly.express as px
-import plotly.io
+import plotly.io as pio
 from skimage import filters, measure, restoration, io, morphology
 
 def apply_processing(img_path):
@@ -14,14 +14,6 @@ def apply_processing(img_path):
     y0, y1, x0, x1 = 0, 400, 0, 1024
 
     image_sequence = image_sequence[:, y0:y1, x0:x1]
-
-    fig = px.imshow(
-        image_sequence,
-        animation_frame=0,
-        binary_string=True,
-        labels={'animation_frame': 'time point'},
-    )
-    plotly.io.show(fig)
 
     # Average and remove background
     def remove_background(image):
@@ -35,7 +27,7 @@ def apply_processing(img_path):
     image_deltas = smoothed[1:, :, :] - smoothed[:-1, :, :]
 
     # Clip lowest and highest intensities
-    p_low, p_high = np.percentile(image_deltas, [5, 70])
+    p_low, p_high = np.percentile(image_deltas, [5, 80])
     clipped = image_deltas - p_low
     clipped[clipped < 0.0] = 0.0
     clipped = clipped / p_high
@@ -49,51 +41,45 @@ def apply_processing(img_path):
 
     # Binarize
     thresh_val = filters.threshold_li(denoised)
-    binarized = denoised > thresh_val
+    binarized = denoised > thresh_val  
 
-    # Select largest region
-    labeled_0 = measure.label(binarized[0, :, :])
-    props_0 = measure.regionprops_table(labeled_0, properties=('label', 'area', 'bbox'))
-    props_0_df = pd.DataFrame(props_0)
-    props_0_df = props_0_df.sort_values('area', ascending=False)
+    def draw_bounding_box_and_dimensions(image, region_props, i):
+        minr, minc, maxr, maxc = (region_props[f'bbox-{j}'] for j in range(4))
+        height = maxr - minr
+        width = maxc - minc
 
-    props_0_df.head()
-
-    largest_region_0 = labeled_0 == props_0_df.iloc[0]['label']
-    
-    # Find the convex hull and create a mask
-    chull = morphology.convex_hull_image(largest_region_0)
-    contours = measure.find_contours(chull)
-
-    # Draw a bounding box
-    minr, minc, maxr, maxc = (props_0_df.iloc[0][f'bbox-{i}'] for i in range(4))
-    height = maxr - minr
-    width = maxc - minc
-
-    fig = px.imshow(
-        vis_sequence, 
-        animation_frame=0, 
-        binary_string=True
+        fig = px.imshow(vis_sequence[i], binary_string=True)
+        fig.add_shape(
+            type='rect', 
+            x0=minc, y0=minr, x1=maxc, y1=maxr, 
+            line=dict(color='Red')
         )
-    fig.add_shape(
-        type='rect', 
-        x0=minc, y0=minr, x1=maxc, y1=maxr, 
-        line=dict(color='Red')
-        )
-    fig.add_annotation(
+        fig.add_annotation(
             text=f'Height: {height}px, Width: {width}px',
             x=minc - 10, y=minr - 10, showarrow=False,
             font=dict(color="Red", size=12),
             align="left"
         )
-    plotly.io.show(fig)          
+        fig.show()
+
+    # Process each image delta
+    for i in range(len(binarized)):
+        labeled = measure.label(binarized[i, :, :])
+        props = measure.regionprops_table(labeled, properties=('label', 'area', 'bbox'))
+        props_df = pd.DataFrame(props)
+        props_df = props_df.sort_values('area', ascending=False)
+
+        if not props_df.empty:
+            largest_region = labeled == props_df.iloc[0]['label']
+            chull = morphology.convex_hull_image(largest_region)
+            draw_bounding_box_and_dimensions(chull, props_df.iloc[0], i)       
 
 def main(input_dir):
     apply_processing(input_dir)
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="Apply image processing to a directory of images using a configuration file.")
-    parser.add_argument('input_dir', type=str, help="Path to directory containing images.")
+    parser = argparse.ArgumentParser(description="Apply image processing to a directory of .tif images.")
+    parser.add_argument('input_dir', type=str, help="Path to directory containing .tif images.")
     args = parser.parse_args()
 
     main(args.input_dir)
